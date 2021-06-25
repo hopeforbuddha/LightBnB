a59b993974bedb6037330e82a02e46012f121468
Jacob <jacob.fitzgerald@outlook.com>
2021-06-25 20:52:59 +0000

    add lightbnb web app

--- 1_queries/listing_city.sql
+++ 1_queries/listing_city.sql
@@ -1,8 +1,8 @@
 select properties.*, avg(property_reviews.rating) as average_rating
 from properties
 join property_reviews on properties.id = property_id
-where city like '%tor%'
+where city like ''
 group by properties.id
 having avg(property_reviews.rating) >= 4
 order by cost_per_night
-limit 5;
+limit 10;
